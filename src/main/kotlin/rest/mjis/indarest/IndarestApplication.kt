2efb8e77c429ff116dd8f11e82d1d7b2ab4600e2
injuk <ingnoh1001@gmail.com>
2024-04-03 20:04:52 +0900

    init: 프로젝트 스캐폴딩

---
+++ src/main/kotlin/rest/mjis/indarest/IndarestApplication.kt
@@ -0,0 +1,11 @@
+package rest.mjis.indarest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IndarestApplication
+
+fun main(args: Array<String>) {
+	runApplication<IndarestApplication>(*args)
+}
---
+++ src/test/kotlin/rest/mjis/indarest/IndarestApplicationTests.kt
@@ -0,0 +1,13 @@
+package rest.mjis.indarest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IndarestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
