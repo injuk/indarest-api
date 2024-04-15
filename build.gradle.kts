8d1f547cb9612b2dafbeb3e4ea5eb57ff930e709
injuk <ingnoh1001@gmail.com>
2024-04-15 22:05:39 +0900

    build: build.gradle 작성 중

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.23"
 	kotlin("plugin.spring") version "1.9.23"
+
+	id("org.jooq.jooq-codegen-gradle") version "3.19.3"
 }
 
 group = "rest.mjis"
@@ -18,14 +20,33 @@ repositories {
 	mavenCentral()
 }
 
+val postgresqlVersion = "42.7.2"
+val r2dbcPostgresqlVersion = "1.0.4.RELEASE"
+val minioVersion = "8.5.9"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.hateoas:spring-hateoas")
+
+	runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
+	runtimeOnly("org.postgresql:r2dbc-postgresql:$r2dbcPostgresqlVersion")
+
+	implementation("org.jooq:jooq")
+	implementation("org.jooq:jooq-kotlin")
+	implementation("org.jooq:jooq-kotlin-coroutines")
+
+	jooqCodegen("jakarta.xml.bind:jakarta.xml.bind-api:4.0.1")
+	jooqCodegen("org.jooq:jooq-meta-extensions")
+	jooqCodegen("org.jooq:jooq-meta-kotlin")
+	jooqCodegen("org.postgresql:postgresql:$postgresqlVersion")
+
+	implementation("io.minio:minio:$minioVersion")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
@@ -40,4 +61,40 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	configuration { }
+
+	executions {
+		create("main") {
+			configuration {
+				logging = org.jooq.meta.jaxb.Logging.DEBUG
+				jdbc {
+					driver = "org.postgresql.Driver"
+					url = "jdbc:postgresql://127.0.0.1:35432/indarest"
+					user = "postgres"
+					password = "my-first-nest-pw"
+				}
+				generator {
+					name = "org.jooq.codegen.KotlinGenerator"
+					database {
+						name = "org.jooq.meta.postgres.PostgresDatabase"
+						inputSchema = "public"
+						excludes = "flyway_schema_history.*"
+					}
+					generate {
+						this.isImplicitJoinPathsAsKotlinProperties = false
+					}
+					target {
+						packageName = "rest.mjis.indarest.persistence.postgresql.jooq"
+						directory = "$projectDir/generated/jooq/src/main"
+					}
+					strategy {
+						name = "org.jooq.codegen.DefaultGeneratorStrategy"
+					}
+				}
+			}
+		}
+	}
 }
