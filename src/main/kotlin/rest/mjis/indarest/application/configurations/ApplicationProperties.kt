adcd1b1f9d8dd7c19eaabc11fb1ed9bf09a7f57e
injuk <ingnoh1001@gmail.com>
2024-04-20 21:02:57 +0900

    feat: application property 수정

--- src/main/kotlin/rest/mjis/indarest/application/configurations/ApplicationProperties.kt
+++ src/main/kotlin/rest/mjis/indarest/application/configurations/ApplicationProperties.kt
@@ -4,9 +4,17 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "storage")
 data class ApplicationProperties(
-    val endpoint: StorageEndpoint,
+    val endpoint: String,
+    val bucket: String,
+    val path: StoragePath,
+    val credential: StorageCredential,
 ) {
-    data class StorageEndpoint(
+    data class StoragePath(
         val pins: String,
+    )
+
+    data class StorageCredential(
+        val accessKey: String,
+        val secretKey: String,
     )
 }
