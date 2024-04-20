cf0a9e647b58a328946fdd985995b074df700860
injuk <ingnoh1001@gmail.com>
2024-04-20 22:40:12 +0900

    test: create pin 유즈케이스 테스트 코드 작성

--- src/test/kotlin/rest/mjis/indarest/application/useCases/commands/CreatePinImplTest.kt
+++ src/test/kotlin/rest/mjis/indarest/application/useCases/commands/CreatePinImplTest.kt
@@ -1,28 +1,128 @@
 package rest.mjis.indarest.application.useCases.commands
 
 import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import rest.mjis.indarest.application.configurations.ApplicationProperties
+import rest.mjis.indarest.application.gateways.clients.StorageClient
+import rest.mjis.indarest.application.gateways.dataAccesses.PinsDataAccess
+import rest.mjis.indarest.domain.AffectedRows
+import rest.mjis.indarest.domain.PinsSearch
 import rest.mjis.indarest.domain.User
+import rest.mjis.indarest.domain.models.Pin
 import rest.mjis.indarest.domain.useCases.CreatePin
+import rest.mjis.indarest.domain.useCases.UpdatePin
 
 @ActiveProfiles("local")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CreatePinImplTest {
+    companion object {
+        private const val VALID_ENDPOINT_PREFIX = "http://localhost:9000/indarest-resources"
+        private const val VALID_OBJECT_KEY = "pins/valid-object-key.png"
+        private const val VALID_RESOURCE_URL = "$VALID_ENDPOINT_PREFIX/$VALID_OBJECT_KEY"
+        private const val CREATED_PIN_ID = 1L
+    }
+
     @Autowired
-    lateinit var useCase: CreatePin
+    lateinit var properties: ApplicationProperties
+
+    private val storageClient: StorageClient = object : StorageClient {
+        override suspend fun isExists(key: String): Boolean {
+            return key == VALID_OBJECT_KEY
+        }
+    }
+
+    private val pinsDataAccess: PinsDataAccess = object : PinsDataAccess {
+        override suspend fun insert(user: User, data: CreatePin.Request): Long {
+            return CREATED_PIN_ID
+        }
+
+        override suspend fun findBy(search: PinsSearch): List<Pin.Summary> {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun findOne(id: Long): Pin {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun update(id: Long, data: UpdatePin.Request): AffectedRows {
+            TODO("Not yet implemented")
+        }
+
+        override suspend fun delete(id: Long): AffectedRows {
+            TODO("Not yet implemented")
+        }
+    }
 
     @Test
-    fun temp(): Unit = runBlocking {
-        val invalidRequest = CreatePin.Request(
-            name = "안뇽",
-            description = null,
-            resourceUrl = "http://localhost:9000/indarest-resources/pins/001/puppy.png",
+    fun `핀 생성시 Long 형태의 id가 반환된다`(): Unit = runBlocking {
+        // given
+        val user = User.create()
+        val request = CreatePin.Request(
+            name = "테스트용 핀",
+            description = "테스트용 핀입니다.",
+            resourceUrl = VALID_RESOURCE_URL,
         )
-        val temp = useCase.execute(User.create(), invalidRequest)
 
-        println(temp)
+        // when
+        val result = CreatePinImpl(
+            properties = properties,
+            storageClient = storageClient,
+            pinsDataAccess = pinsDataAccess,
+        ).execute(user, request)
+
+        // then
+        assertThat(result.id).isEqualTo(CREATED_PIN_ID)
+    }
+
+    @Test
+    fun `resourceUrl이 지정된 오브젝트 스토리지가 아닌 경우 예외가 발생한다`(): Unit = runBlocking {
+        // given
+        val user = User.create()
+        val request = CreatePin.Request(
+            name = "테스트용 핀",
+            description = "테스트용 핀입니다.",
+            resourceUrl = "https://my-s3-bucket.com/hello.png",
+        )
+
+        // when
+        val result = assertThrows<IllegalArgumentException> {
+            CreatePinImpl(
+                properties = properties,
+                storageClient = storageClient,
+                pinsDataAccess = pinsDataAccess,
+            ).execute(user, request)
+        }
+
+        // then
+        assertThat(result.message).isEqualTo("resource url has invalid storage endpoint")
+    }
+
+    @Test
+    fun `존재하지 않는 오브젝트를 지정하여 핀을 생성할 경우 예외가 발생한다`(): Unit = runBlocking {
+        // given
+        val invalidObjectKey = "pins/invalid-object.png"
+        val user = User.create()
+        val request = CreatePin.Request(
+            name = "테스트용 핀",
+            description = "테스트용 핀입니다.",
+            resourceUrl = "$VALID_ENDPOINT_PREFIX/$invalidObjectKey",
+        )
+
+        // when
+        val result = assertThrows<RuntimeException> {
+            CreatePinImpl(
+                properties = properties,
+                storageClient = storageClient,
+                pinsDataAccess = pinsDataAccess,
+            ).execute(user, request)
+        }
+
+        // then
+        assertThat(result.message).isEqualTo("resource($invalidObjectKey) not found")
     }
 }
