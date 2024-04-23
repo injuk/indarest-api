7ab9f703d570f25e2019e2ed4ce07af9ab6c7c45
injuk <ingnoh1001@gmail.com>
2024-04-23 19:17:43 +0900

    feat: storage client가 upload url을 생성할 수 있도록 개선

--- src/main/kotlin/rest/mjis/indarest/application/gateways/clients/StorageClient.kt
+++ src/main/kotlin/rest/mjis/indarest/application/gateways/clients/StorageClient.kt
@@ -1,6 +1,7 @@
 package rest.mjis.indarest.application.gateways.clients
 
 interface StorageClient {
-    suspend fun isExists(key: String): Boolean
-    suspend fun deleteAll(keys: List<String>)
+    suspend fun isExists(objectKey: String): Boolean
+    suspend fun deleteAll(objectKeys: List<String>)
+    suspend fun createUploadUrl(objectKey: String): String
 }
--- src/main/kotlin/rest/mjis/indarest/infrastructure/storage/MinioClient.kt
+++ src/main/kotlin/rest/mjis/indarest/infrastructure/storage/MinioClient.kt
@@ -1,10 +1,12 @@
 package rest.mjis.indarest.infrastructure.storage
 
+import io.minio.GetPresignedObjectUrlArgs
 import io.minio.MinioAsyncClient
 import io.minio.MinioClient
 import io.minio.RemoveObjectsArgs
 import io.minio.StatObjectArgs
 import io.minio.errors.ErrorResponseException
+import io.minio.http.Method
 import io.minio.messages.DeleteObject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.future.await
@@ -16,6 +18,7 @@ import reactor.kotlin.core.publisher.toFlux
 import rest.mjis.indarest.application.configurations.ApplicationProperties
 import rest.mjis.indarest.application.gateways.clients.StorageClient
 import java.util.concurrent.CompletionException
+import java.util.concurrent.TimeUnit
 
 @Component
 class MinioClient(
@@ -29,19 +32,19 @@ class MinioClient(
             .build()
     }
 
-    override suspend fun isExists(key: String): Boolean = withContext(Dispatchers.IO) {
+    override suspend fun isExists(objectKey: String): Boolean = withContext(Dispatchers.IO) {
         try {
             val response = client.statObject(
                 StatObjectArgs.builder()
                     .bucket(properties.bucket)
-                    .`object`(key)
+                    .`object`(objectKey)
                     .build()
             )
                 .await()
 
             logger.info("statObject returns object($response)")
 
-            return@withContext response.`object`() == key
+            return@withContext response.`object`() == objectKey
         } catch (e: CompletionException) {
             val minioException = e.cause?.cause
 
@@ -54,7 +57,7 @@ class MinioClient(
                 throw e
             }
 
-            logger.error("there is no object($key)")
+            logger.error("there is no object($objectKey)")
 
             return@withContext false
         } catch (e: Exception) {
@@ -63,16 +66,31 @@ class MinioClient(
         }
     }
 
-    override suspend fun deleteAll(keys: List<String>): Unit = withContext(Dispatchers.IO) {
-        logger.info("try to delete objects($keys)")
+    override suspend fun deleteAll(objectKeys: List<String>): Unit = withContext(Dispatchers.IO) {
+        logger.info("try to delete objects($objectKeys)")
         val response = client.removeObjects(
             RemoveObjectsArgs.builder()
                 .bucket(properties.bucket)
                 .objects(
-                    keys.map { DeleteObject(it) }
+                    objectKeys.map { DeleteObject(it) }
                 )
                 .build()
         ).toFlux().collectList().awaitFirst()
         logger.info("removeObjects command returns response: $response")
+    }
+
+    override suspend fun createUploadUrl(objectKey: String): String {
+        logger.info("try to get presigned url for objectKey($objectKey)")
+        val response = client.getPresignedObjectUrl(
+            GetPresignedObjectUrlArgs.builder()
+                .method(Method.PUT)
+                .bucket(properties.bucket)
+                .`object`(objectKey)
+                .expiry(5, TimeUnit.MINUTES)
+                .build()
+        )
+        logger.info("getPresignedObjectUrl command returns response: $response")
+
+        return response
     }
 }
